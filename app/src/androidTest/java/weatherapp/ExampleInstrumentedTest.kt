3fec1d60b3fed0d5f4249a5aeb077d48a1ca7b4c
Aashish1107 <55628840+Aashish1107@users.noreply.github.com>
2022-11-13 21:19:19 +0530

    Update ExampleInstrumentedTest.kt

--- app/src/androidTest/java/weatherapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/weatherapp/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("rakib.hasan.weatherapp", appContext.packageName)
+        assertEquals("weatherapp", appContext.packageName)
     }
 }
